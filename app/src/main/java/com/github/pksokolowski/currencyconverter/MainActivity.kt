cc5c43a26c35747f72183f1ef087991b222be98e
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2022-03-19 22:01:18 +0100

    remove preview for MainActivity

--- app/src/main/java/com/github/pksokolowski/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/github/pksokolowski/currencyconverter/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.pksokolowski.currencyconverter.ui.screens.ConverterScreen
 import com.github.pksokolowski.currencyconverter.ui.theme.CurrencyConverterTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,20 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CurrencyConverterTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     ConverterScreen()
                 }
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CurrencyConverterTheme {
-        ConverterScreen()
     }
 }
